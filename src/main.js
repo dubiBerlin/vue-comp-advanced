4fd6f695c0f821fdfbf26106de346501170a9384
Dubravko <woose@web.de>
2019-02-28 17:59:10 +0100

    a local filter added

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
 import AppAufgabe from "./AppAufgabe.vue";
+import AppDirectives from "./AppDirectives.vue";
+import AppMixinsFilters from "./AppMixinsFilters.vue";
+
+Vue.filter("to-lowercase");
 
 new Vue({
   el: "#app",
-  render: h => h(AppAufgabe)
+  render: h => h(AppMixinsFilters)
 });
