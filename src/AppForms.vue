007340b7ce8debaed6816882c822a0f0405ed108
Dubravko <woose@web.de>
2019-03-11 09:31:17 +0100

    select in forms

--- src/AppForms.vue
+++ src/AppForms.vue
@@ -58,8 +58,8 @@
       <div class="row">
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group">
           <label for="priority">Priority</label>
-          <select id="priority" class="form-control">
-            <option></option>
+          <select id="priority" class="form-control" v-model="selectedPriority">
+            <option v-for="priority in priorities" :selected="priority === 'Medium'">{{priority}}</option>
           </select>
         </div>
       </div>
@@ -110,7 +110,9 @@ export default {
       },
       message: "A new text",
       sendMail: [], // die values von checkboxen werden im array gespeichert durch v-model
-      gender: "Male"
+      gender: "Male",
+      priorities: ["High", "Medium", "Low"],
+      selectedPriority="High"
     };
   }
 };
