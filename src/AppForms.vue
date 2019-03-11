3b1730ffd18246f3408f9aab3f6facd11ac43de6
Dubravko <woose@web.de>
2019-03-11 08:55:31 +0100

    v-model for textarea

--- src/AppForms.vue
+++ src/AppForms.vue
@@ -30,7 +30,7 @@
           <label for="message">Message</label>
           <br>
           <!-- Interpolation between <textarea>{{ test }}</textarea> doesn't work!-->
-          <textarea id="message" rows="5" class="form-control"></textarea>
+          <textarea id="message" rows="5" class="form-control" v-model="message"></textarea>
         </div>
       </div>
       <div class="row">
@@ -81,7 +81,7 @@
             <p>Mail: {{userData.email}}</p>
             <p>Password: {{userData.password}}</p>
             <p>Age: {{userData.age}}</p>
-            <p>Message:</p>
+            <p>Message: {{message}}</p>
             <p>
               <strong>Send Mail?</strong>
             </p>
@@ -106,7 +106,8 @@ export default {
         email: "",
         password: "",
         age: 27
-      }
+      },
+      message: "A new text"
     };
   }
 };
