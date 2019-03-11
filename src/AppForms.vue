2f09b074d020f87af329594355d9ef55c957d88f
Dubravko <woose@web.de>
2019-03-11 09:12:56 +0100

    radiobox

--- src/AppForms.vue
+++ src/AppForms.vue
@@ -48,10 +48,10 @@
       <div class="row">
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
           <label for="male">
-            <input type="radio" id="male" value="Male"> Male
+            <input type="radio" id="male" value="Male" v-model="gender"> Male
           </label>
           <label for="female">
-            <input type="radio" id="female" value="Female"> Female
+            <input type="radio" id="female" value="Female" v-model="gender"> Female
           </label>
         </div>
       </div>
@@ -89,7 +89,7 @@
             <ul>
               <li></li>
             </ul>
-            <p>Gender:</p>
+            <p>Gender:{{gender}}</p>
             <p>Priority:</p>
             <p>Switched:</p>
           </div>
@@ -109,7 +109,8 @@ export default {
         age: 27
       },
       message: "A new text",
-      sendMail: [] // die values von checkboxen werden im array gespeichert durch v-model
+      sendMail: [], // die values von checkboxen werden im array gespeichert durch v-model
+      gender: "Male"
     };
   }
 };
