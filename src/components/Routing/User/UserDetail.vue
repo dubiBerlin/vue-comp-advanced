e412094ada5dcbb3f6f57567a180a16b4ae20843
Dubravko <woose@web.de>
2019-04-03 16:33:47 +0200

    access routes by name

--- src/components/Routing/User/UserDetail.vue
+++ src/components/Routing/User/UserDetail.vue
@@ -2,6 +2,10 @@
   <div>
     <h3>Some User Details</h3>
     <p>Loaded User Id : {{$route.params.id}}</p>
-    <router-link tag="button" :to="'/user/'+$route.params.id+'/edit'" class="btn btn-primary">Edit</router-link>
+    <router-link
+      tag="button"
+      :to="{name:'userEdit',params:{id:$route.params.id}}"
+      class="btn btn-primary"
+    >Edit</router-link>
   </div>
 </template>
